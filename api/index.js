39b53b0a8c8cd594bb2262ff32739960874afd5b
Romadhan Prasetyo <romadhanedy@gmail.com>
2024-01-29 20:19:06 +0700

    auth signup

--- api/index.js
+++ api/index.js
@@ -1,21 +1,36 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log("Error: ", err);
-});
+  });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+  console.log("Server is running on port 3000");
+});
 
-app.use("/", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
